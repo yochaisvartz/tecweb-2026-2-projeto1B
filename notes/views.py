86bcc259219cb2eebb583fc38ed3d21b94b095ff
yochaisvartz <yochais@al.insper.edu.br>
2026-09-02 08:19:37 -0300

    handout feito

---
+++ notes/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from .models import Note
+
+
+def index(request):
+    if request.method == 'POST':
+        title = request.POST.get('titulo')
+        content = request.POST.get('detalhes')
+        Note.objects.create(title=title, content=content)
+        return redirect('index')
+    else:
+        all_notes = Note.objects.all()
+        return render(request, 'notes/index.html', {'notes': all_notes})
